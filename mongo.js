b84217324fedd7adfc194a28e5bde360f3d2f834
yu <mark2020.yu@gmail.com>
2022-04-10 23:00:52 +0800

    practice kata -- todos & mongoose

---
+++ mongo.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+// const DB = "mongodb://localhost:27017/todo";
+const DB = process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD);
+
+mongoose
+	.connect(DB)
+	.then(() => {
+		console.log("資料庫連線成功");
+	})
+	.catch((error) => {
+		console.log(error);
+	});

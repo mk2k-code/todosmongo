b97911a207e90c4617ec162682abecd1e9a31f9c
yu <mark2020.yu@gmail.com>
2022-04-23 05:14:20 +0800

    add myApp

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
-const headers  = require("../util/httpHeader");
+const fetch = require("cross-fetch");
+const headers = require("../util/httpHeader");
 const { errorHandle, resWriteData } = require("../util/httpMsg");
 require("../db");
 const Todo = require("../models/todo");
@@ -7,11 +8,11 @@ const Post = require("../models/post");
 const todosControllers = require("../controllers/todos");
 const postsControllers = require("../controllers/posts");
 
-todos =[];
-posts =[];
+todos = [];
+posts = [];
 const routes = async (req, res) => {
-    const { url, method } = req;
-    console.log(method, url);
+	const { url, method } = req;
+	console.log(method, url);
 
 	let body = "";
 	req.on("data", (chunk) => {
@@ -21,56 +22,66 @@ const routes = async (req, res) => {
 	if (req.url.startsWith("/todos")) {
 		if (req.url == "/todos" && req.method == "GET") {
 			todosControllers.getTodos(req, res, todos);
-	
 		} else if (req.url.startsWith("/todos/") && req.method == "GET") {
 			todosControllers.getTodo(req, res, todos);
-	
 		} else if (req.url == "/todos" && req.method == "POST") {
 			req.on("end", () => {
 				todosControllers.postTodo(req, res, body);
 			});
-	
 		} else if (req.url == "/todos" && req.method == "DELETE") {
 			todosControllers.delTodos(req, res, todos);
-	
 		} else if (req.url.startsWith("/todos/") && req.method == "DELETE") {
 			todosControllers.delTodo(req, res);
-	
 		} else if (req.url.startsWith("/todos/") && req.method == "PATCH") {
 			req.on("end", () => {
 				todosControllers.patchTodo(req, res, body);
 			});
-
+		} else if ((req.method = "OPTIONS")) {
+			res.writeHead(200, headers);
+			res.end();
 		} else {
 			errorHandle(req, res, 404);
 		}
-
 	} else if (req.url.startsWith("/posts")) {
-
 		if (req.url == "/posts" && req.method == "GET") {
 			postsControllers.getPosts(req, res, posts);
-	
 		} else if (req.url.startsWith("/posts/") && req.method == "GET") {
 			postsControllers.getPost(req, res, posts);
-	
 		} else if (req.url == "/posts" && req.method == "POST") {
 			req.on("end", () => {
 				postsControllers.postPost(req, res, body);
 			});
-	
 		} else if (req.url == "/posts" && req.method == "DELETE") {
 			postsControllers.delPosts(req, res, posts);
-	
 		} else if (req.url.startsWith("/posts/") && req.method == "DELETE") {
 			postsControllers.delPost(req, res);
-	
 		} else if (req.url.startsWith("/posts/") && req.method == "PATCH") {
 			req.on("end", () => {
 				postsControllers.patchPost(req, res, body);
 			});
+		} else if ((req.method = "OPTIONS")) {
+			res.writeHead(200, headers);
+			res.end();
 		} else {
 			errorHandle(req, res, 404);
 		}
+	} else if (req.url == "/ip" && req.method == "GET") {
+		const url = "https://api.ipify.org?format=json";
+		const myHost = "polar-reaches-82211.herokuapp.com";
+		console.log("-----");
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				console.log("-----");
+				myApp = {
+					host: myHost,
+					ip: data.ip,
+				};
+				resWriteData(res, myApp);
+			})
+			.catch((error) => console.log(error, 2));
+
 	} else if ((req.method = "OPTIONS")) {
 		res.writeHead(200, headers);
 		res.end();
@@ -78,9 +89,6 @@ const routes = async (req, res) => {
 	} else {
 		errorHandle(req, res, 404);
 	}
-
-
-
 };
 
 module.exports = routes;

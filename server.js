e70ab41d36f35e70bc7f5504dc7747caffb14623
yu <mark2020.yu@gmail.com>
2022-04-23 16:32:21 +0800

    modify server.js

--- server.js
+++ server.js
@@ -2,13 +2,9 @@ const http = require("http");
 const routes = require("./routes");
 const port = process.env.PORT || 3006;
 
-
 const app = async (req, res) => {
 	routes(req, res);
 };
-console.log(`HEROKU_APP_NAME: ${process.env.HEROKU_APP_NAME}`);
-console.log(`process.env.REDISTOGO_URL: ${process.env.REDISTOGO_URL}`);
-
 
 const server = http.createServer(app);
 server.listen(port, () => {
